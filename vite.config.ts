77955a37a111dcc0517a6842e1b9945246650d11
Jhonatan <jhonatanmorenoloayza@gmail.com>
2022-08-05 11:37:03 -0500

    feat: add aliases to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+import path from "path";
+
+const getPath = (route) => path.resolve(__dirname, route);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@src": getPath("./src/"),
+      "@components": getPath("./src/shared/components/"),
+      "@shared": getPath("./src/shared/"),
+      "@routes": getPath("./src/routes/"),
+      "@styles": getPath("./src/styles/"),
+      "@pages": getPath("./src/pages/"),
+      "@store": getPath("./src/store/"),
+      "@services": getPath("./src/services/"),
+    },
+  },
+});
