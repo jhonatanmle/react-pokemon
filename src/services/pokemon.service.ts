3a469c2ad82a8d3c01e032aaaca781335e494da7
Jhonatan <jhonatanmorenoloayza@gmail.com>
2022-08-05 00:30:45 -0500

    feat: create service with axios configuration

---
+++ src/services/pokemon.service.ts
@@ -0,0 +1,25 @@
+import { Pokemon } from "../shared/models/pokemon.model";
+import { httpClient } from "./config";
+
+export const getPokemonsService = async (): Promise<Pokemon[]> => {
+  try {
+    const { data } = await httpClient.get("/pokemon?limit=151");
+
+    return data.results;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const getPokemonDetailService = async (
+  name: string
+): Promise<Pokemon | null> => {
+  try {
+    const { data } = await httpClient.get(`/pokemon/${name}`);
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
